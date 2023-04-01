da655acc11974b9e66e418ce4e3a33ebff1905ef
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-01 17:45:16 +0800

    extract words from wikipedia most searched english articles

---
+++ data/data.py
@@ -0,0 +1,33 @@
+import json
+import pandas as pd
+from utils import get_words_from
+
+def wikipedia_page(name):
+    return f"https://en.wikipedia.org/wiki/{name}".replace(" ", "_")
+
+# CSV with the most viewed articles
+file = "data/top-wikipedia-articles-2023_02.csv"
+
+# Get all most searched wikipedia pages urls
+pages_names = pd.read_csv(file)['Page'].tolist()
+pages_urls = [wikipedia_page(x) for x in pages_names]
+
+# Initialize word counts dictionary
+word_counts = {}
+
+# Count all occurrences of words in all pages
+for page_url in pages_urls:
+    words = get_words_from(page_url)
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+# Keep only those words that appeared at least 100 times
+useful_words = []
+for key in word_counts.keys():
+    if word_counts[key] >= 100:
+        useful_words.append(key)
+
+# Save useful words into a json file
+with open("data/queries.json", "w") as f:
+    json.dump(useful_words, f)
+
---
+++ data/utils.py
@@ -0,0 +1,41 @@
+import urllib.request
+import re
+import nltk
+from nltk.corpus import stopwords
+
+def remove_stopwords(word_list):
+
+    # Get the set of English stop words
+    stop_words = set(stopwords.words('english'))
+
+    # Remove stop words from the list of words
+    filtered_words = [word for word in word_list if not word in stop_words]
+
+    return filtered_words
+
+
+def get_words_from(url):
+    try:
+        response = urllib.request.urlopen(url) # Download the HTML
+        html = response.read().decode() # Decode the HTML from bytes to string
+
+        # Define the regex pattern to match all words in the body
+        pattern = re.compile(r'<body.*?>(.*?)</body>', re.DOTALL | re.IGNORECASE)
+        body_html = pattern.search(html).group(1)
+
+        # Extract all words from the body HTML using the regex pattern
+        words = re.findall(r'\b\w+\b', body_html)
+
+        # Filter words
+        filtered_words = remove_stopwords(words)
+
+        # Print the words
+        print(f"Retrieved {len(words)}. Remaining {len(filtered_words)} filtered words. Page: {url}")
+
+        return filtered_words
+
+    except:
+        print("Not able to retrieve data from", url)
+        return []
+
+
