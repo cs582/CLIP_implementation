ae3749e8cb9ee371842aa2cbf102463ef5b88a65
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-03 00:00:51 +0800

    added option to load from in word scrapping

--- data/nlp/word_scraping.py
+++ data/nlp/word_scraping.py
@@ -1,6 +1,4 @@
-import json
 import nltk
-import pandas as pd
 from utils import extract_words_from_pages_in_dump_file
 
 # Download stopwords
@@ -10,7 +8,7 @@ nltk.download('stopwords')
 file = "data/nlp/wkpages/enwiki-latest-pages-articles-multistream10.xml-p4045403p5399366"
 
 # Extract words from pages in the dumpfile
-words = extract_words_from_pages_in_dump_file(file)
+words = extract_words_from_pages_in_dump_file(file, start_from=7900000)
 
 # Final console message
 print(f"finished scrapping with {len(words)} words.")
