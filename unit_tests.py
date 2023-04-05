4ee00ce5aab8a1228520a7296a90445c06f7019e
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-05 13:09:47 +0800

    added unit tests for transformer

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
-from src.tests.unit_tests import UtilsTest
+from src.tests.cv_unit_tests import UtilsTest
+from src.tests.nlp_unit_tests import TransformerRadfordUnitTest
 
 unittest.main()
