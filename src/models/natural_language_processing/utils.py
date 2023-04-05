aad2200533bbd0a3f89cb94aa02eca974ef08cf3
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-05 17:19:36 +0800

    from scratch implementation of transformers

--- src/models/natural_language_processing/utils.py
+++ src/models/natural_language_processing/utils.py
@@ -3,39 +3,42 @@ import torch.nn as nn
 
 
 class ScaledDotProductAttention(nn.Module):
-    def __init__(self, word_dim):
+    def __init__(self):
         super(ScaledDotProductAttention, self).__init__()
-        self.dim = torch.tensor(word_dim)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, q, k, v, mask):
-        mul = torch.bmm(q.transpose(1, 2), k) / torch.sqrt(self.dim)
-        mul[mask] = -1000
+        mul = torch.bmm(q, k.transpose(1, 2)) / (len(q[0])**0.5)
+        mul[mask] = -1000.0
         out = self.softmax(mul)
-        print("out shape", out.shape)
-        out = torch.bmm(out, v.transpose(1, 2))
+        out = torch.bmm(out, v)
         return out
 
+
 class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, dim, nhead):
+    def __init__(self, embedd_dim, vector_size, nhead):
         super(MultiHeadSelfAttention, self).__init__()
-        self.dim = dim
+        self.dim = vector_size
+        self.embedd_dim = embedd_dim
         self.nhead = nhead
 
         assert self.dim % self.nhead == 0, "dim must be a multiple of nhead"
 
         self.dim_proj = self.dim//self.nhead
 
-        self.q_projs = [nn.Linear(self.dim, self.dim_proj) for _ in range(self.nhead)]
-        self.k_projs = [nn.Linear(self.dim, self.dim_proj) for _ in range(self.nhead)]
-        self.v_projs = [nn.Linear(self.dim, self.dim_proj) for _ in range(self.nhead)]
+        self.q_projs = [nn.Linear(self.embedd_dim, self.dim_proj) for _ in range(self.nhead)]
+        self.k_projs = [nn.Linear(self.embedd_dim, self.dim_proj) for _ in range(self.nhead)]
+        self.v_projs = [nn.Linear(self.embedd_dim, self.dim_proj) for _ in range(self.nhead)]
 
-        self.heads = [ScaledDotProductAttention(dim=self.dim_proj) for _ in range(self.nhead)]
+        self.heads = [ScaledDotProductAttention() for _ in range(self.nhead)]
 
-    def forward(self, q, k, v):
-        out = torch.concat(torch.tensor([
-            head(self.q_projs[i](q), self.k_projs[i](k), self.v_projs[i](v)) for i, head in enumerate(self.heads)
-        ]))
+    def forward(self, q, k, v, mask):
+        tensors = []
+        for i in range(0, self.nhead):
+            q_i, k_i, v_i = self.q_projs[i](q), self.k_projs[i](k), self.v_projs[i](v)
+            head_i = self.heads[i](q_i, k_i, v_i, mask)
+            tensors.append(head_i)
+        out = torch.cat(tensors, dim=2)
         return out
 
 
--- src/tests/nlp_unit_tests.py
+++ src/tests/nlp_unit_tests.py
@@ -1,6 +1,7 @@
 import unittest
 import torch
 import time
+import numpy as np
 
 from src.models.natural_language_processing.utils import ScaledDotProductAttention
 from src.models.natural_language_processing.utils import MultiHeadSelfAttention
@@ -9,31 +10,47 @@ from src.models.natural_language_processing.utils import MultiHeadSelfAttention
 class TransformerRadfordUnitTest(unittest.TestCase):
     def test_scaled_dot_product_attention(self):
         # batch of 8 with 75 words of dim 25
-        q = torch.rand(8, 75, 25)
-        k = torch.rand(8, 75, 25)
-        v = torch.rand(8, 75, 25)
+        batch_size = 32
 
-        mask = torch.rand(8, 25, 25) < 0.1
+        q = torch.rand(batch_size, 75, 25)
+        k = torch.rand(batch_size, 75, 25)
+        v = torch.rand(batch_size, 75, 25)
+
+        # Fake a mask of words
+        mask = torch.ones(batch_size, 75, dtype=torch.bool)
+        for w in range(0, 75):
+            mask[:, np.random.randint(low=75//2, high=75):] = 0.0
 
         start = time.time()
-        model = ScaledDotProductAttention(dim=25)
+        model = ScaledDotProductAttention()
         end = time.time()
-        self.assertEqual(model(q, k, v, mask).shape, (8, 25, 75))
+        self.assertEqual(model(q, k, v, mask).shape, (batch_size, 75, 25), msg="Dot Product Attention Failed")
 
         print(f"Scaled Dot Product Attention Masked finished in {end-start} seconds")
 
     def test_multi_head_self_attention(self):
-        # batch of 8 with 75 words of dim 25
-        q = torch.rand(8, 75, 25)
-        k = torch.rand(8, 75, 25)
-        v = torch.rand(8, 75, 25)
+        # batch of 8 with 75 words embedded on 25 dim
+        num_words = 75
+        batch_size = 32
+        embedding_dim = 25
+        latent_vector_size = 512
 
-        mask = torch.rand(8, 25) < 0.1
+        # nhead
+        nhead = 8
+
+        q = torch.rand(batch_size, num_words, embedding_dim)
+        k = torch.rand(batch_size, num_words, embedding_dim)
+        v = torch.rand(batch_size, num_words, embedding_dim)
+
+        # Fake a mask of words
+        mask = torch.ones(batch_size, num_words, dtype=torch.bool)
+        for w in range(0, num_words):
+            mask[:, np.random.randint(low=num_words//2, high=num_words):] = 0.0
 
         start = time.time()
-        model = MultiHeadSelfAttention(dim=64, nhead=8)
+        model = MultiHeadSelfAttention(embedd_dim=embedding_dim, vector_size=latent_vector_size, nhead=nhead)
         end = time.time()
 
-        self.assertEqual(model(q, k, v, mask).shape, (8, 75, 68))
+        self.assertEqual(model(q, k, v, mask).shape, (batch_size, num_words, latent_vector_size), msg="Self Attention Failed")
 
         print(f"Multi-head Masked Self-Attention finished in {end-start} seconds")
