2772b9fee20b7cc2d6baffb81edef39d513cbe1a
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-11 19:17:07 +0800

    added option to run gpu tests

--- unit_tests.py
+++ unit_tests.py
@@ -5,6 +5,7 @@ from src.tests.cv_modules_unit_tests import ResnetModulesUnitTest
 from src.tests.nlp_unit_tests import TransformerRadfordUnitTest
 from src.tests.image_scraping_unit_tests import ImageScrappingUnitTests
 from src.tests.cv_backbones_unit_tests import BackbonesUnitTest
+from src.tests.cv_backbones_unit_tests import BackbonesUnitTestGPU
 
 
 parser = argparse.ArgumentParser(
@@ -14,6 +15,8 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('-heavy', type=bool, default=False, help='Include heavy tasks such as backbones in the unit test.')
+parser.add_argument('-use_gpu', type=bool, default=False, help='Test backbones on GPU, heavy must be set to True.')
+
 
 args = parser.parse_args()
 
@@ -33,12 +36,19 @@ if __name__ == '__main__':
     # Test Backbones
     backbones_test = unittest.TestLoader().loadTestsFromTestCase(BackbonesUnitTest)
 
+    # Test Backbones on GPU
+    backbones_test_gpu = unittest.TestLoader().loadTestsFromTestCase(BackbonesUnitTestGPU)
+
     # List all tests to run
     tests_to_run = [utils_test, RN_modules_test, radford_test, image_scrapping_test]
 
     if args.heavy:
         print("Stacked Backbones Tests to TestSuite...")
         tests_to_run.append(backbones_test)
+
+        if args.use_gpu:
+            print("Stacked Backbones Tests with GPU to TestSuite...")
+            tests_to_run.append(backbones_test_gpu)
 
     # Test suite that includes all the tests
     suite = unittest.TestSuite(tests_to_run)
