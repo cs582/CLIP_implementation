79feac21fc2b62969939e649b7459c25f54e266e
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-11 20:35:56 +0800

    ViT@224 AND ViT@336 implemented

--- src/models/computer_vision/backbones/vit.py
+++ src/models/computer_vision/backbones/vit.py
@@ -4,48 +4,121 @@ import torch.nn as nn
 from einops.layers.torch import Rearrange
 
 
-# ViT (Dosovitskiy et. al. 2020)
-class ViT(nn.Module):
-    def __init__(self, patch_resolution, img_size, n_classes):
-        super(ViT, self).__init__()
-        self.h, self.w = img_size
-        self.p = patch_resolution
+# ViT (Dosovitskiy et. al. 2020) / CLIP (Radford et al. 2021)
+# ViT-L/14 @ 224
+class ViTat224(nn.Module):
+    def __init__(self, embedding_dim):
+        super(ViTat224, self).__init__()
+        # ViT Hyper-parameters
+        self.n_channels, self.h, self.w = (3, 224, 224)
+        self.p = 14
 
-        self.n_channels = 3
+        # Number of layers
+        self.n_layers = 24
+        # Latent vector size D
+        self.vector_size = 1024
+        # FF dim
+        self.mlp_size = 4096
+        # Number of heads
+        self.nhead = 16
 
         # Number of patches
         self.n_embeddings = (self.h * self.w) // (self.p**2)
-
-        # Latent vector size D
-        vector_size = 512
-
-        nhead = 16
-        mlp_size = 2048
 
         # Get patches
         self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=self.p, p2=self.p)
 
         # Patch embedding
-        self.patch_embedding_encoder = nn.Parameter(torch.randn(1, self.n_embeddings, vector_size))
+        self.patch_embedding_encoder = nn.Parameter(torch.randn(1, self.n_embeddings, self.vector_size))
         # Class token
-        self.class_token = nn.Parameter(torch.randn(1, 1, vector_size))
+        self.class_token = nn.Parameter(torch.randn(1, 1, self.vector_size))
         # Position embedding
-        self.pos_embedding = nn.Parameter(torch.randn(1, self.n_embeddings + 1, vector_size))
+        self.pos_embedding = nn.Parameter(torch.randn(1, self.n_embeddings + 1, self.vector_size))
 
         # Layer normalization as in CLIP (Radford et al. 2021)
-        self.layer_norm = nn.LayerNorm(vector_size)
+        self.layer_norm = nn.LayerNorm(self.vector_size)
 
         # Transformer Encoder Hidden Layers
-        self.t_encoder1 = TransformerEncoderBlock(vector_size=vector_size, nhead=nhead, mlp_dim=mlp_size)
-        self.t_encoder2 = TransformerEncoderBlock(vector_size=vector_size, nhead=nhead, mlp_dim=mlp_size)
-        self.t_encoder3 = TransformerEncoderBlock(vector_size=vector_size, nhead=nhead, mlp_dim=mlp_size)
+        self.transformer_layer = nn.TransformerEncoderLayer(d_model=self.vector_size, nhead=self.nhead)
+        self.transformer = nn.TransformerEncoder(encoder_layer=nn.TransformerEncoderLayer(), num_layers=self.n_layers)
         self.to_latent = nn.Identity()
 
         # Out MLP head with one
         self.mlp_head = nn.Sequential(
-            nn.LayerNorm(vector_size),
-            nn.Linear(vector_size, mlp_size),
-            nn.Linear(mlp_size, n_classes),
+            nn.LayerNorm(self.vector_size),
+            nn.Linear(self.vector_size, self.mlp_size),
+            nn.Linear(self.mlp_size, embedding_dim),
+        )
+
+    def forward(self, x):
+        N, c, h, w = x.shape
+
+        # Convert image to patches
+        x = self.rearrange(x)
+
+        # Patch Embedding
+        x = torch.matmul(x, self.patch_embedding_encoder)
+        class_token = self.class_token.expand(N, -1, -1)
+        x = torch.cat((class_token, x), dim=1)
+        x += self.pos_embedding
+        x = self.layer_norm(x)
+
+        # Transformer Encoder Layers
+        x = self.t_encoder1(x)
+        x = self.t_encoder2(x)
+        x = self.t_encoder3(x)
+
+        # Getting class token
+        x = x[:, 0]
+        x = self.to_latent(x)
+
+        x = self.mlp_head(x)
+        return x
+
+
+# ViT-L/14 @ 336
+class ViTat336(nn.Module):
+    def __init__(self, embedding_dim):
+        super(ViTat336, self).__init__()
+        # ViT Hyper-parameters
+        self.n_channels, self.h, self.w = (3, 336, 336)
+        self.p = 14
+
+        # Number of layers
+        self.n_layers = 24
+        # Latent vector size D
+        self.vector_size = 1024
+        # FF dim
+        self.mlp_size = 4096
+        # Number of heads
+        self.nhead = 16
+
+        # Number of patches
+        self.n_embeddings = (self.h * self.w) // (self.p**2)
+
+        # Get patches
+        self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=self.p, p2=self.p)
+
+        # Patch embedding
+        self.patch_embedding_encoder = nn.Parameter(torch.randn(1, self.n_embeddings, self.vector_size))
+        # Class token
+        self.class_token = nn.Parameter(torch.randn(1, 1, self.vector_size))
+        # Position embedding
+        self.pos_embedding = nn.Parameter(torch.randn(1, self.n_embeddings + 1, self.vector_size))
+
+        # Layer normalization as in CLIP (Radford et al. 2021)
+        self.layer_norm = nn.LayerNorm(self.vector_size)
+
+        # Transformer Encoder Hidden Layers
+        self.transformer_layer = nn.TransformerEncoderLayer(d_model=self.vector_size, nhead=self.nhead)
+        self.transformer = nn.TransformerEncoder(encoder_layer=nn.TransformerEncoderLayer(), num_layers=self.n_layers)
+        self.to_latent = nn.Identity()
+
+        # Out MLP head with one
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(self.vector_size),
+            nn.Linear(self.vector_size, self.mlp_size),
+            nn.Linear(self.mlp_size, embedding_dim),
         )
 
     def forward(self, x):
--- src/tests/cv_backbones_unit_tests.py
+++ src/tests/cv_backbones_unit_tests.py
@@ -3,6 +3,7 @@ import torch
 import time
 
 from src.models.computer_vision.backbones.resnet34 import RN34at224, RN34at336
+from src.models.computer_vision.backbones.vit import ViTat224, ViTat336
 
 
 class BackbonesUnitTest(unittest.TestCase):
@@ -31,6 +32,32 @@ class BackbonesUnitTest(unittest.TestCase):
         print(message)
 
         self.assertEqual(out.shape, (4, 1000), msg=f"RN@336 Failed, out size {out.shape} should be (2,1000)")
+
+    def test_ViT_at_224(self):
+        x = torch.rand(4, 3, 224, 224)
+        model = ViTat224(embedding_dim=768)
+
+        start = time.time()
+        out = model(x)
+        end = time.time()
+
+        message = f"ViT-L/14@224 forward time: {end - start} seconds"
+        print(message)
+
+        self.assertEqual(out.shape, (4, 768), msg=f"ViT-L/14@224 Failed, out size {out.shape} should be (4, 768)")
+
+    def test_ViT_at_336(self):
+        x = torch.rand(4, 3, 336, 336)
+        model = ViTat336(embedding_dim=768)
+
+        start = time.time()
+        out = model(x)
+        end = time.time()
+
+        message = f"ViT-L/14@336 forward time: {end - start} seconds"
+        print(message)
+
+        self.assertEqual(out.shape, (4, 768), msg=f"ViT-L/14@336 Failed, out size {out.shape} should be (4, 768)")
 
 
 class BackbonesUnitTestGPU(unittest.TestCase):
@@ -63,3 +90,34 @@ class BackbonesUnitTestGPU(unittest.TestCase):
         print(message)
 
         self.assertEqual(out.shape, (64, 1000), msg=f"RN@336 Failed, out size {out.shape} should be (64, 1000)")
+
+    def test_ViT_at_224(self):
+        device = torch.device('cuda:0')
+
+        x = torch.rand(4, 3, 224, 224).to(device)
+        model = ViTat224(embedding_dim=768).to(device)
+
+        start = time.time()
+        out = model(x)
+        end = time.time()
+
+        message = f"ViT-L/14@224 forward time: {end - start} seconds"
+        print(message)
+
+        self.assertEqual(out.shape, (4, 768), msg=f"ViT-L/14@336 Failed, out size {out.shape} should be (4, 768)")
+
+    def test_ViT_at_336(self):
+        device = torch.device('cuda:0')
+
+        x = torch.rand(4, 3, 336, 336).to(device)
+        model = ViTat336(embedding_dim=768).to(device)
+
+        start = time.time()
+        out = model(x)
+        end = time.time()
+
+        message = f"ViT-L/14@336 forward time: {end - start} seconds"
+        print(message)
+
+        self.assertEqual(out.shape, (4, 768), msg=f"ViT-L/14@336 Failed, out size {out.shape} should be (4, 768)")
+
