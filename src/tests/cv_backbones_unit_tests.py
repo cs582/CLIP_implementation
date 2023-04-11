422ef6408d806ccf71679ee2914ad2dfa65c3454
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-11 19:16:30 +0800

    included tests on GPU

--- src/tests/cv_backbones_unit_tests.py
+++ src/tests/cv_backbones_unit_tests.py
@@ -2,7 +2,7 @@ import unittest
 import torch
 import time
 
-from src.models.computer_vision.cv_backbones import RN34_at224, RN34_at336
+from src.models.computer_vision.backbones.resnet34 import RN34_at224, RN34_at336
 
 
 class BackbonesUnitTest(unittest.TestCase):
@@ -28,6 +28,38 @@ class BackbonesUnitTest(unittest.TestCase):
         end = time.time()
 
         message = f"RN@336 forward time: {end - start} seconds"
+        print(message)
+
+        self.assertEqual(out.shape, (4, 1000), msg=f"RN@336 Failed, out size {out.shape} should be (2,1000)")
+
+
+class BackbonesUnitTestGPU(unittest.TestCase):
+    def test_RN_at_224(self):
+        device = torch.device('cuda:0')
+
+        x = torch.rand(4, 3, 224, 224).to(device)
+        model = RN34_at224(embedding_dim=1000).to(device)
+
+        start = time.time()
+        out = model(x)
+        end = time.time()
+
+        message = f"RN@224 [CUDA: {torch.cuda.get_device_name(0)}] forward time: {end - start} seconds"
+        print(message)
+
+        self.assertEqual(out.shape, (4, 1000), msg=f"RN@224 Failed, out size {out.shape} should be (2,1000)")
+
+    def test_RN_at_336(self):
+        device = torch.device('cuda:0')
+
+        x = torch.rand(4, 3, 336, 336).to(device)
+        model = RN34_at336(embedding_dim=1000).to(device)
+
+        start = time.time()
+        out = model(x)
+        end = time.time()
+
+        message = f"RN@336 [CUDA: {torch.cuda.get_device_name(0)}] forward time: {end - start} seconds"
         print(message)
 
         self.assertEqual(out.shape, (4, 1000), msg=f"RN@336 Failed, out size {out.shape} should be (2,1000)")
