a6ff7a39d9459a3fe1465aad5dbd79a7342033c0
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-12 14:07:07 +0800

    implementation of TransformerDecoder (Radford 2019) for NLP

--- src/models/natural_language_processing/nlp_modules.py
+++ src/models/natural_language_processing/nlp_modules.py
@@ -1,12 +1,92 @@
+import torch
 import torch.nn as nn
 
-class TransformerEncoderRadford(nn.Module):
-    def __init__(self, latent_vs, nhead, mlp_dim, num_layers):
-        super(TransformerEncoderRadford, self).__init__()
 
-        encoder_layer = nn.TransformerEncoderLayer(d_model=latent_vs, nhead=nhead, dim_feedforward=mlp_dim)
-        self.transformer = nn.TransformerEncoder(encoder_layer=encoder_layer, num_layers=num_layers, mask_check=True)
+class TransformerRadford(nn.Module):
+    def __init__(self, dim_model, nhead, dim_ff):
+        super(TransformerRadford, self).__init__()
+        self.masked_self_attention = MaskedMultiHeadSelfAttention(dim_model=dim_model, n_head=nhead)
+        self.layer_norm_att = nn.LayerNorm(dim_model)
+
+        self.mlp = MultilayerPerceptron(dim_model=dim_model, dim_ff=dim_ff)
+        self.layer_norm_mlp = nn.LayerNorm(dim_model)
 
     def forward(self, x, mask):
-        return self.transformer(x, mask)
+        x = torch.add(self.masked_self_attention(x, mask), x)
+        x = self.layer_norm_att(x)
+        x = torch.add(self.mlp(x), x)
+        out = self.layer_norm_mlp(x)
+        return out
+
+
+class MultilayerPerceptron(nn.Module):
+    def __init__(self, dim_model, dim_ff):
+        super(MultilayerPerceptron, self).__init__()
+        self.dim_model = dim_model
+        self.dim_ff = dim_ff
+
+        self.fc_hidden = nn.Linear(self.dim_model, self.dim_ff)
+        self.fc_out = nn.Linear(self.dim_ff, self.dim_model)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc_hidden(x))
+        x = self.fc_out(x)
+        return x
+
+
+class MaskedMultiHeadSelfAttention(nn.Module):
+    def __init__(self, dim_model, n_head):
+        super(MaskedMultiHeadSelfAttention, self).__init__()
+
+        assert dim_model % n_head == 0, "dim_att must be a multiple of n_head"
+
+        self.dim_model = dim_model
+        self.dim_att = dim_model // n_head
+
+        self.n_head = n_head
+
+        self.attention_heads = [
+            MaskedSelfAttention(dim_x=self.dim_model, dim_att=self.dim_att) for _ in range(self.n_head)
+            ]
+
+        self.fc = nn.Linear(self.dim_model, self.dim_model)
+
+    def forward(self, x, mask):
+        x_heads = [None] * self.n_head
+        for i, head in enumerate(self.attention_heads):
+            x_heads[i] = head(x, mask)
+        x = torch.cat(x_heads, dim=2)
+        x = self.fc(x)
+        return x
+
+
+class MaskedSelfAttention(nn.Module):
+    def __init__(self, dim_x, dim_att):
+        super(MaskedSelfAttention, self).__init__()
+
+        self.dim_x = dim_x
+
+        self.dim_k = dim_att
+        self.dim_v = dim_att
+
+        self.wq = nn.Parameter(torch.rand(1, self.dim_x, self.dim_k))
+        self.wk = nn.Parameter(torch.rand(1, self.dim_x, self.dim_k))
+        self.wv = nn.Parameter(torch.rand(1, self.dim_x, self.dim_v))
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x, mask=None): # b x l_max x dim_v
+        q = torch.matmul(x, self.wq) # b x l_max x dim_q
+        k = torch.matmul(x, self.wk) # b x l_max x dim_k
+        v = torch.matmul(x, self.wv) # b x l_max x dim_v
+
+        s = torch.bmm(q, k.transpose(1, 2)) # b x l_max x l_max
+        s[mask] = -1000.0
+        s = s / (self.dim_k ** 0.5) # regularization
+        s = self.softmax(s)
+        out = torch.bmm(s, v)
+        return out
+
 
