b94eaaacb32ba383e98cb5c3f0561825a803e27d
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-12 22:03:43 +0800

    Implemented CLIP core module

---
+++ src/models/CLIP_model.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+
+class CLIPModule(nn.Module):
+    def __init__(self, image_encoder, text_encoder, dim_img, dim_text, number_of_pairs, temperature):
+        super(CLIPModule, self).__init__()
+        # The embedding dimension is equal to the number of image-queries to pair in the training stage
+        self.embedding_dim = number_of_pairs
+        self.dim_img = dim_img
+        self.dim_text = dim_text
+
+        self.temperature = temperature
+
+        self.image_encoder = image_encoder(dim_out=self.dim_img)
+        self.text_encoder = text_encoder(dim_out=self.dim_text)
+
+        self.img_mm_encoder = nn.Parameter(torch.randn(1, self.dim_img, self.embedding_dim))
+        self.txt_mm_encoder = nn.Parameter(torch.randn(1, self.dim_text, self.embedding_dim))
+
+    def forward(self, image, text):
+        # Extract feature representation of each modality
+        img_f = self.image_encoder(image) # batch_size x dim_img
+        txt_f = self.text_encoder(text)   # batch_suze x dim_text
+
+        # Joint multimodal embedding
+        img_e = torch.norm(torch.matmul(img_f, self.img_mm_encoder), p=2, dim=1) # batch_size x embedding_dim
+        txt_e = torch.norm(torch.matmul(txt_f, self.txt_mm_encoder), p=2, dim=1) # batch_size x embedding_dim
+
+        # Scaled pairwise cosine similarities
+        logits = torch.matmul(img_e, txt_e.transpose(0, 1)) * torch.exp(self.temperature)
+        return logits
+
+
+
+
+
+
+
+
+
+
+
