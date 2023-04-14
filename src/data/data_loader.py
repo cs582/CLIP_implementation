cf0afbfc68db2bfc8c492e92ae1c1365a8a34bf5
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-15 01:07:41 +0800

    dataloader

---
+++ src/data/data_loader.py
@@ -0,0 +1,37 @@
+import json
+import os
+import torch
+import cv2
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
+
+
+class ImageQueryDataset(Dataset):
+    def __init__(self, data_dir, filename, img_res=(224, 224)):
+        self.data_dir = data_dir
+        self.transform = transforms.Compose([
+            transforms.RandomCrop(img_res),
+            transforms.ToTensor()
+        ])
+
+        # Get a list of image file paths
+        with open(f"{data_dir}/{filename}", 'r') as f:
+            self.image_files = json.loads(f)
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        # Load the image at the given index
+        image_path = self.image_files[index]
+        image_full_path = f"{self.data_dir}/{image_path}"
+        image = cv2.imread(image_full_path)
+
+        # Apply any data transformations if specified
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Get the label from the image file name
+        label = image_path.replace('.jpg', '').replace('_', ' ')
+
+        return image, label
