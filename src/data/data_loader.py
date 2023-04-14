b64583a0165a56902e9419d37476da33fe869856
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-15 01:59:22 +0800

    logs

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,13 +1,21 @@
 import json
-import os
-import torch
 import cv2
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision.transforms import transforms
+
+
+def resize_image(image, x, y):
+    height, width = image.shape[:2]
+    ratio = max(x/width, y/height)
+    new_width = int(width * ratio)
+    new_height = int(height * ratio)
+    resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+    return resized_image
 
 
 class ImageQueryDataset(Dataset):
     def __init__(self, data_dir, filename, img_res=(224, 224)):
+        self.img_res = img_res
         self.data_dir = data_dir
         self.transform = transforms.Compose([
             transforms.ToTensor(),
@@ -26,6 +34,9 @@ class ImageQueryDataset(Dataset):
         image_path = self.image_files[index]
         image_full_path = f"{self.data_dir}/images/{image_path}"
         image = cv2.imread(image_full_path)
+
+        if image.shape[0] < self.img_res[0] or image.shape[1] < self.img_res[1]:
+            image = resize_image(image, *self.img_res)
 
         # Apply any data transformations if specified
         if self.transform is not None:
