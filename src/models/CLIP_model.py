Fixed normalization

src/models/CLIP_model.py
@@ -25,10 +25,10 @@ class CLIPModule(nn.Module):
 
         # Joint multimodal embedding
         img_e = torch.matmul(img_f, self.img_mm_encoder) # batch_size x dim_emb
-        img_e = img_e / img_e.norm(p=2, dim=0)
+        img_e = img_e / img_e.norm(p=2, dim=0) # l2 normalization
 
         txt_e = torch.matmul(txt_f, self.txt_mm_encoder) # batch_size x dim_emb
-        txt_e = txt_e / txt_e.norm(p=2, dim=0)
+        txt_e = txt_e / txt_e.norm(p=2, dim=0) # l2 normalization
 
         # Scaled pairwise cosine similarities
         logits = torch.matmul(img_e, txt_e.transpose(0,1)) * torch.exp(torch.tensor(self.temperature)) # batch_size x batch_size