11997af7e59dd48f1d649cfe35d7ce4dd8cdc69f
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-15 21:27:49 +0800

    added another log

--- src/models/natural_language_processing/nlp_backbones.py
+++ src/models/natural_language_processing/nlp_backbones.py
@@ -101,6 +101,7 @@ class TextTransformer(nn.Module):
         # Get last [EOS] token
         print("pre last word", x.shape)
         last_word = torch.cat((mask, torch.zeros(b, 1, dtype=torch.bool, device=mask.device)), dim=1).diff(dim=1) # last word mask
+        print(last_word)
         print("last word mask", last_word.shape)
         x = x[last_word]
         print("x after", x.shape)
