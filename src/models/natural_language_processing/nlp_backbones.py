8a5c6c8f68b957991f37314bad0fa6392f06624c
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-15 21:40:21 +0800

    everything good with the models

--- src/models/natural_language_processing/nlp_backbones.py
+++ src/models/natural_language_processing/nlp_backbones.py
@@ -19,18 +19,14 @@ class TransformerB(nn.Module):
         self.register_buffer('mask', mask)
 
     def forward(self, x):
-        print("\nx in", x.shape)
         # Mask
         self.mask = (x != -1)
         # Set other tokens to zero
         x[~self.mask] = 0.0
-        print("after setting -1 to 0.0", x.shape)
         # Token embedder
         x = self.token_embedder(x)
-        print("after token embedding", x.shape)
         # Transformer backbone
         x = self.transformer(x, self.mask)
-        print("after transformer", x.shape)
         return x
 
 
@@ -48,18 +44,14 @@ class TransformerL(nn.Module):
         self.register_buffer('mask', mask)
 
     def forward(self, x):
-        print("\nx in", x.shape)
         # Mask
         self.mask = (x != -1)
         # Set other tokens to zero
         x[~self.mask] = 0.0
-        print("after setting -1 to 0.0", x.shape)
         # Token embedder
         x = self.token_embedder(x)
-        print("after token embedding", x.shape)
         # Transformer backbone
         x = self.transformer(x, self.mask)
-        print("after transformer", x.shape)
         return x
 
 
