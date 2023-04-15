5304b96b013ceb7a8b146cfc5aad1692e5cc808d
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-15 22:22:21 +0800

    dot product

--- src/models/CLIP_model.py
+++ src/models/CLIP_model.py
@@ -25,10 +25,12 @@ class CLIPModule(nn.Module):
 
         # Joint multimodal embedding
         img_e = torch.matmul(img_f, self.img_mm_encoder) # batch_size x dim_emb
-        img_e = img_e / img_e.norm(p=2, dim=1)
+        print("img_e", img_e.shape)
+        img_e = img_e / img_e.norm(p=2, dim=0)
 
         txt_e = torch.matmul(txt_f, self.txt_mm_encoder) # batch_size x dim_emb
-        txt_e = txt_e / txt_e.norm(p=2, dim=1)
+        print("txt_e", txt_e.shape)
+        txt_e = txt_e / txt_e.norm(p=2, dim=0)
 
         # Scaled pairwise cosine similarities
         print(torch.dot(img_e, txt_e).shape)
