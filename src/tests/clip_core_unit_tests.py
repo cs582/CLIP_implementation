dddbdb8cb6c887dc01aa817807478532bb930232
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-15 22:30:33 +0800

    added GPU tesg=t

--- src/tests/clip_core_unit_tests.py
+++ src/tests/clip_core_unit_tests.py
@@ -51,5 +51,43 @@ class CLIPUnitTest(unittest.TestCase):
 
 
 
+class CLIPGPUUnitTest(unittest.TestCase):
+    def test_clip_module_with_ViTat224_in_gpu(self):
+        device = torch.device("cuda:0")
 
+        # CLIP parameters
+        batch_size = 8
+        embedding_dim = 512
+        temperature = 0.07
+
+        # Text Encoder parameters
+        vocab_size = 32000
+        max_length = 24
+        dim_text = 512
+
+        # Image Encoder parameters
+        dim_img = 768
+
+        # Initialize encoders
+        image_encoder = ViTat224(dim_out=dim_img)
+        text_encoder = TransformerB(dim_out=dim_text, batch_size=batch_size, vocab_size=vocab_size, max_length=max_length)
+
+        # Initialize CLIP
+        clip_model = CLIPModule(image_encoder, text_encoder, dim_img, dim_text, embedding_dim, temperature).to(device)
+
+        # Testing images
+        resolution = 224
+        imgs = torch.rand(batch_size, 3, resolution, resolution).to(device)
+        tokenized_words = torch.randint(low=0, high=1, size=(batch_size, max_length)).to(device)
+        for q_idx in range(len(tokenized_words)):
+            tokenized_words[q_idx, np.random.randint(low=1, high=max_length):] = -1.0
+
+        # Output
+        start = time.time()
+        out = clip_model(imgs, tokenized_words)
+        end = time.time()
+
+        # Assert case
+        self.assertEqual(out.shape, (batch_size, batch_size))
+        print(f"CLIP forward finished in {end-start} seconds with a batch of {batch_size}.")
 
