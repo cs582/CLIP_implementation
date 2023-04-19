836d72d1cb9110151468cb926f800309396b2764
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-20 00:00:42 +0800

    unit tests for tokenization

--- src/tests/nlp_tokenization_unit_tests.py
+++ src/tests/nlp_tokenization_unit_tests.py
@@ -32,3 +32,21 @@ class TokenizationUnitTest(unittest.TestCase):
         print("tokenized: ", test_sentence, " -> ", tokens)
         print(f"Tokenizer training done in {end-start} seconds.")
 
+    def test_saving_tokenizer(self):
+
+        tokenizer = BytePairEncoderTokenizer(vocab_size=100, min_freq=2)
+
+        body_text = [
+            "This is the test for the Byte Pair Encoder Tokenizer",
+            "CLIP is an AI developed by OpenAI",
+            "It is used for text and image feature extraction in Dalle-2",
+            "But is also used in other algorithms f.e. Stable Diffusion 1 and 2",
+            "It was also implemented fr0m scratch, hopefully, it works just fine."
+            "We hope this is a good test case."
+        ]
+
+        tokenizer.train(body_text)
+        tokenizer.save_tokenizer("src/data/nlp/tokenizers", test_mode=True)
+
+
+
