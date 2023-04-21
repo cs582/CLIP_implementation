eb2df20f705aa3fc8b51d6ab64b2055ffcd3ed37
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-21 14:48:38 +0800

    fixed different models

--- src/tests/clip_core_unit_tests.py
+++ src/tests/clip_core_unit_tests.py
@@ -55,50 +55,11 @@ class CLIPUnitTest(unittest.TestCase):
 
 
 class CLIPGPUUnitTest(unittest.TestCase):
-    def test_clip_module_with_ViTat224_in_gpu(self):
-        device = torch.device("cuda:0")
-
-        # CLIP parameters
-        batch_size = 32
-        embedding_dim = 512
-        temperature = 0.07
-
-        # Text Encoder parameters
-        vocab_size = 32000
-        max_length = 24
-        dim_text = 512
-
-        # Image Encoder parameters
-        dim_img = 768
-
-        # Initialize encoders
-        image_encoder = ViTat224(dim_out=dim_img)
-        text_encoder = TransformerB(dim_out=dim_text, batch_size=batch_size, vocab_size=vocab_size, max_length=max_length)
-
-        # Initialize CLIP
-        clip_model = CLIPModule(image_encoder, text_encoder, dim_img, dim_text, embedding_dim, temperature).to(device)
-
-        # Testing images
-        resolution = 224
-        imgs = torch.rand(batch_size, 3, resolution, resolution).to(device)
-        tokenized_words = torch.randint(low=1, high=vocab_size, size=(batch_size, max_length)).to(device)
-        for q_idx in range(len(tokenized_words)):
-            tokenized_words[q_idx, np.random.randint(low=1, high=max_length):] = 0.0
-
-        # Output
-        start = time.time()
-        out = clip_model(imgs, tokenized_words)
-        end = time.time()
-
-        # Assert case
-        self.assertEqual(out.shape, (batch_size, batch_size))
-        print(f"CLIP [CUDA: {torch.cuda.get_device_name(0)}] forward finished in {end-start} seconds with a batch of {batch_size}.")
-
     def test_backward_clip_module_with_ViTat224_in_gpu(self):
         device = torch.device("cuda:0")
 
         # CLIP parameters
-        batch_size = 32
+        batch_size = 64
         embedding_dim = 512
         temperature = 0.07
 
@@ -160,13 +121,13 @@ class CLIPGPUUnitTest(unittest.TestCase):
         # Assert case
         self.assertEqual(logits.shape, (batch_size, batch_size), msg=f"logits shape {logits.shape} doesn't match expected size ({batch_size}, {batch_size})")
         self.assertNotEqual(loss.item(), torch.nan, msg=f"Loss item is {loss.item()}")
-        print(f"CLIP [CUDA: {torch.cuda.get_device_name(0)}] backward finished in {end_time-start_time} seconds with a batch of {batch_size}.")
+        print(f"CLIP ViT@224 [CUDA: {torch.cuda.get_device_name(0)}] backward finished in {end_time-start_time} seconds with a batch of {batch_size}.")
 
     def test_backward_clip_module_with_ViTat112_in_gpu(self):
         device = torch.device("cuda:0")
 
         # CLIP parameters
-        batch_size = 64
+        batch_size = 112
         embedding_dim = 512
         temperature = 0.07
 
@@ -228,4 +189,4 @@ class CLIPGPUUnitTest(unittest.TestCase):
         # Assert case
         self.assertEqual(logits.shape, (batch_size, batch_size), msg=f"logits shape {logits.shape} doesn't match expected size ({batch_size}, {batch_size})")
         self.assertNotEqual(loss.item(), torch.nan, msg=f"Loss item is {loss.item()}")
-        print(f"CLIP [CUDA: {torch.cuda.get_device_name(0)}] backward finished in {end_time-start_time} seconds with a batch of {batch_size}.")
+        print(f"CLIP ViT@112 [CUDA: {torch.cuda.get_device_name(0)}] backward finished in {end_time-start_time} seconds with a batch of {batch_size}.")
