353f158242c06033daf7a15181dc557fe6ab899a
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-24 15:16:25 +0800

    loading images unit tests

--- src/tests/loading_images_unit_test.py
+++ src/tests/loading_images_unit_test.py
@@ -26,7 +26,7 @@ class LoadingImagesUnitTest(unittest.TestCase):
 
             times[i] = end - start
 
-        print(f"Loaded images with PIL: {times}. AVG: {np.mean(times[1:])}")
+        print(f"Loaded images with PIL, avg time: {np.mean(times[1:])} x {x.shape}")
 
     def test_cv2(self):
         img_path = "src/data/image_gen/WQ-dataset/images"
@@ -42,6 +42,6 @@ class LoadingImagesUnitTest(unittest.TestCase):
 
             times[i] = end - start
 
-        print(f"Loaded images with OpenCV: {times}. AVG: {np.mean(times[1:])}")
+        print(f"Loaded images with OpenCV, avg time: {np.mean(times[1:])}. x {x.shape}")
 
 
