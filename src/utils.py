15ea9d26252a4ca9e764a03f7be4a4385a211242
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-04-25 17:22:58 +0800

    training log message

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def training_info_log_message(device, epochs, batch_size, image_encoder, text_encoder, image_dim_out, text_dim_out, optimizer):
+
+    text = f"""
+    CLIP TRAINING
+    ____________________________________________
+    Device:         {torch.cuda.get_device_name() if device == torch.device("cuda:0") else "CPU"}
+    Epochs:         {epochs}
+    Batch size:     {batch_size}
+    Image Encoder:  {image_encoder} (dim = {image_dim_out})
+    Text Encoder:   {text_encoder}  (dim = {text_dim_out})
+    Optimizer:      {optimizer}
+    _____________________________________________
+    """
+
+    print(text)
