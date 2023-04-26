fixed scaling with temperature parameter and adjusted accordingly at CLIP_model.py and trainer.py

src/models/CLIP_Loss.py
@@ -8,13 +8,13 @@ class CLIPLoss(nn.Module):
         self.logits_length = logits_length
         self.register_buffer("labels", torch.arange(0, self.logits_length))
 
-    def forward(self, logits):
+    def forward(self, logits_images, logits_text):
         # Define loss for each task
         images_loss = nn.CrossEntropyLoss()
         text_loss = nn.CrossEntropyLoss()
 
         # Loss function
-        loss_images = images_loss(logits, self.labels)
-        loss_text = text_loss(logits.transpose(0, 1), self.labels)
+        loss_images = images_loss(logits_images, self.labels)
+        loss_text = text_loss(logits_text, self.labels)
         loss = (loss_images + loss_text)/2
         return loss

src/models/CLIP_model.py
@@ -33,9 +33,11 @@ class CLIPModule(nn.Module):
         txt_e = F.normalize(txt_e, p=2, dim=1) # l2 normalization
 
         # Scaled pairwise cosine similarities
-        logits = torch.matmul(img_e, txt_e.t())  # batch_size x batch_size
-        logits = logits * torch.exp(self.temperature) # Scale by temperature
-        return logits
+        logits_images = torch.matmul(img_e, txt_e.t())  # batch_size x batch_size
+        logits_images = logits_images * torch.exp(self.temperature) # Scale by temperature
+        logits_text = logits_images.t()
+
+        return logits_images, logits_text
 
 
 