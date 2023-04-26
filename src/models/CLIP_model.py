fixed scaling features using temperature

src/models/CLIP_model.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -11,7 +12,7 @@ class CLIPModule(nn.Module):
         self.dim_img = dim_img
         self.dim_text = dim_text
 
-        self.temperature = torch.tensor(temperature, requires_grad=True)
+        self.temperature = torch.tensor(np.log(1 / temperature), requires_grad=True)
 
         self.image_encoder = image_encoder
         self.text_encoder = text_encoder
@@ -31,12 +32,9 @@ class CLIPModule(nn.Module):
         txt_e = torch.matmul(txt_f, self.txt_mm_encoder) # batch_size x dim_emb
         txt_e = F.normalize(txt_e, p=2, dim=1) # l2 normalization
 
-        # Max value of the logits
-        clip_upper = self.temperature * 100.0
-
         # Scaled pairwise cosine similarities
-        logits = torch.matmul(img_e, txt_e.transpose(0,1))  # batch_size x batch_size
-        logits = torch.minimum(logits * torch.exp(self.temperature), clip_upper) # Scale by temerature
+        logits = torch.matmul(img_e, txt_e.t())  # batch_size x batch_size
+        logits = logits * torch.exp(self.temperature) # Scale by temperature
         return logits
 
 