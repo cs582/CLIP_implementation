70477bae8b979a2aa28708d42807063cd2305dcc
cs582 <cs582@duke.edu>
2023-05-07 22:00:39 +0800

    saving tools and miscelanous

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,17 @@ def warmup_scheduler(optimizer, warmup_steps, lr_max):
 
 
 def save_checkpoint(model, optimizer, epoch, scheduler, history, models_dir):
+    """
+    Save torch model checkpoint.
+
+    :param model: (torch.nn.Module) Torch model.
+    :param optimizer: (torch.nn.Optimizer) Torch optimizer.
+    :param epoch: (int) Last epoch trained on.
+    :param scheduler: (object) Learning rate scheduler object for warmup.
+    :param history: (list) History loss.
+    :param models_dir: (str) Train models directory.
+    :return: None
+    """
     if not os.path.exists(models_dir):
         os.mkdir(models_dir)
 
@@ -27,6 +38,15 @@ def save_checkpoint(model, optimizer, epoch, scheduler, history, models_dir):
 
 
 def load_from_checkpoint(model_filepath, model, scheduler, optimizer):
+    """
+    Load torch model from given path.
+
+    :param model_filepath: (str) model filepath.
+    :param model: (torch.nn.Module) torch backbone.
+    :param scheduler: (object) learning rate optimizer.
+    :param optimizer: (torch.nn.Optimizer) torch optimizer.
+    :return:
+    """
 
     checkpoint = torch.load(model_filepath)
     model.load_state_dict(checkpoint['model_state_dict'])
@@ -41,6 +61,19 @@ def load_from_checkpoint(model_filepath, model, scheduler, optimizer):
 
 
 def training_info_log_message(device, epochs, batch_size, image_encoder, text_encoder, image_dim_out, text_dim_out, optimizer):
+    """
+    Prints the training loop information.
+
+    :param device: (torch.device) device.
+    :param epochs: (int) number of epochs.
+    :param batch_size: (int) batch size.
+    :param image_encoder: (str) image encoder name.
+    :param text_encoder: (str) text encoder name.
+    :param image_dim_out: (int) image width.
+    :param text_dim_out: (int) text width.
+    :param optimizer: (torch.nn.Optimizer) torch optimizer.
+    :return:
+    """
 
     text = f"""
     CLIP TRAINING
