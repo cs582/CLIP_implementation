1ae378b0201cc5dcfc64d8217bc33de20a50dc54
cs582 <cs582@duke.edu>
2023-05-11 14:07:24 +0800

    Evaluation

---
+++ eval.py
@@ -0,0 +1,97 @@
+import torch
+import argparse
+
+
+from src.eval_loop import eval
+from torch.utils.data import DataLoader
+from src.data.data_loader import ImageQueryDataset
+
+from src.models.CLIP_Loss import CLIPLoss
+from src.models.CLIP_model import CLIPModule
+
+from src.models.computer_vision.backbones.vit import ViTBaseOver16at112, ViTBaseOver32at224, ViTSmallOver8at112, ViTSmallOver16at112
+from src.models.natural_language_processing.nlp_backbones import GPTBase, GPTLarge
+
+from src.utils import training_info_log_message
+
+
+parser = argparse.ArgumentParser(
+    prog='CLIP Evaluator.',
+    description='CLIP evaluation loop.',
+    epilog='The CLIP evaluation loop computes the loss and accuracy across the given dataset.'
+)
+
+# Trainer mode
+parser.add_argument('-device', type=str, default="cpu", help="Set device to use: gpu or cpu.")
+parser.add_argument('-checkpoint', type=bool, default=False, help="Load model from last checkpoint and restart training from there.")
+
+# CLIP Hyper-parameters
+parser.add_argument('-image_encoder', type=str, default=None, help="Image encoder backbone. One of (ViT) @112, @224, or @336.")
+parser.add_argument('-text_encoder', type=str, default=None, help="Text encoder backbone. One of S (Small), B (Base), or L (Large).")
+parser.add_argument('-batch_size', type=int, default=128, help="Batch size. Is the same as the multimodal embedding dimension.")
+parser.add_argument('-vocab_size', type=int, default=43001, help="Vocabulary size from trained tokenizer.")
+parser.add_argument('-max_length', type=int, default=34, help="Max length of the token encoding.")
+parser.add_argument('-text_dim_out', type=int, default=512, help="Text encoder output dimension.")
+parser.add_argument('-image_dim_out', type=int, default=768, help="Image encoder output dimension.")
+parser.add_argument('-embedding_dim', type=int, default=512, help="Embedding dimension CLIP.")
+
+args = parser.parse_args()
+
+dataset_file = "src/data/image_gen/WQ-dataset/WQI_local.csv"
+image_path = "src/data/image_gen/WQ-dataset/images"
+tokenizer_file = "src/data/nlp/tokenizers/CLIP-bpe.tokenizer.json"
+
+
+if __name__ == "__main__":
+
+    # Get multimodal embedding dim which is equal to the batch size
+    multimodal_embedding_dim = args.batch_size
+
+    # Get device
+    device = torch.device('cuda:0') if args.device=="gpu" else torch.device('cpu')
+
+    # Pick Image Encoder model
+    assert args.image_encoder in ['B/32@224', 'B/16@112', 'S/8@112', 'S/16@112']
+
+    # Enable cublasLt for mixed-precision training
+    torch.backends.cudnn.enabled = True
+
+    image_model = None
+    image_resolution = None
+    if args.image_encoder == "B/32@224":
+        image_model = ViTBaseOver32at224(dim_out=args.image_dim_out).to(device)
+        image_resolution = 224
+    if args.image_encoder == "B/16@112":
+        image_model = ViTBaseOver16at112(dim_out=args.image_dim_out).to(device)
+        image_resolution = 112
+    if args.image_encoder == "S/8@112":
+        image_model = ViTSmallOver8at112(dim_out=args.image_dim_out).to(device)
+        image_resolution = 112
+    if args.image_encoder == "S/16@112":
+        image_model = ViTSmallOver16at112(dim_out=args.image_dim_out).to(device)
+        image_resolution = 112
+
+    # Pick Text Encoder model
+    assert args.text_encoder in ['B', 'L']
+
+    text_model = None
+    if args.text_encoder == "B":
+        text_model = GPTBase(dim_out=args.text_dim_out, vocab_size=args.vocab_size, max_length=args.max_length, batch_size=args.batch_size).to(device)
+    if args.text_encoder == "L":
+        text_model = GPTLarge(dim_out=args.text_dim_out, vocab_size=args.vocab_size, max_length=args.max_length, batch_size=args.batch_size).to(device)
+
+    # Load training dataset
+    eval_dataset = ImageQueryDataset(dataset_file, image_path, tokenizer_file, args.max_length, device, start_from=7500000, img_res=image_resolution)
+    dataloader = DataLoader(eval_dataset, batch_size=multimodal_embedding_dim, num_workers=10, drop_last=True)
+
+    # Set CLIP Loss function
+    loss_func = CLIPLoss(logits_length=multimodal_embedding_dim).to(device)
+
+    # Call CLIP core model
+    clip_model = CLIPModule(image_encoder=image_model, text_encoder=text_model, dim_img=args.image_dim_out, dim_text=args.text_dim_out, embedding_dim=args.embedding_dim, temperature=0.07).to(device)
+
+    # Print training information
+    training_info_log_message(device, None, args.batch_size, args.image_encoder, args.text_encoder, args.image_dim_out, args.text_dim_out, None)
+
+    # Training cycle
+    eval(eval_dataset=dataloader, clip_model=clip_model, loss_function=loss_func, device=device, model_name=args.image_encoder, load_from_given_checkpoint=args.checkpoint)
---
+++ src/eval_loop.py
@@ -0,0 +1,47 @@
+import numpy as np
+import boto3
+import json
+import os
+
+from tqdm import tqdm
+from src.utils import save_checkpoint, load_from_checkpoint
+
+
+s3 = boto3.client('s3')
+
+# Models directory
+models_dir = "src/models/checkpoints"
+
+
+def eval(eval_dataset, clip_model, loss_function, device, model_name, load_from_given_checkpoint=None):
+    """
+    Training loop.
+    :param eval_dataset: (Dataloader) training data.
+    :param clip_model: (torch.nn.Module) CLIP model.
+    :param loss_function: (torch.nn.Module) CLIP loss function.
+    :param device: (torch.device) Torch device.
+    :param model_name: (str) Image encoder name.
+    :param load_from_given_checkpoint: (str) Gives it a specific model to load.
+    :return:
+    """
+    _, _ = load_from_checkpoint(load_from_given_checkpoint, clip_model)
+
+    loss_eval = []
+
+    pbar = tqdm(total=len(eval_dataset))
+    for idx, (images, queries) in enumerate(eval_dataset):
+        images, queries = images.to(device), queries.to(device)
+
+        # Extract feature representations
+        logits_images, logits_text = clip_model(images, queries)
+
+        # Compute Loss
+        loss = loss_function(logits_images, logits_text)
+        loss_eval.append(loss.item())
+
+        # Set pbar description
+        pbar.set_description(f"Eval Loop")
+
+        pbar.update(1)
+
+    print(f"Eval Loop DONE! AVG Loss: {np.round(np.mean(loss_eval), 5)}.")
