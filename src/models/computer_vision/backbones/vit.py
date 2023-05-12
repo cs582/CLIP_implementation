123ace7c478b4d43068b97a34daebd796c264bf1
cs582 <cs582@duke.edu>
2023-05-12 12:52:20 +0800

    Added MicroViT/14@112

--- src/models/computer_vision/backbones/vit.py
+++ src/models/computer_vision/backbones/vit.py
@@ -6,6 +6,76 @@ from einops.layers.torch import Rearrange
 
 # ViT (Dosovitskiy et. al. 2020) / CLIP (Radford et al. 2021)
 
+# ViT-M/16 @ 112
+class ViTMicroOver14at112(nn.Module):
+    def __init__(self, dim_out):
+        super(ViTMicroOver14at112, self).__init__()
+        # ViT Hyper-parameters
+        self.c, self.h, self.w = (3, 112, 112)
+        self.p = 14
+
+        # Number of layers
+        self.n_layers = 6
+        # Latent vector size D
+        self.vector_size = 256
+        # FF dim
+        self.mlp_size = 512
+        # Number of heads
+        self.nhead = 8
+
+        # Number of patches
+        self.n_embeddings = (self.h * self.w) // (self.p**2)
+
+        # Get patches
+        self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=self.p, p2=self.p)
+
+        # Patch embedding
+        self.patch_embedding = nn.Parameter(torch.randn(1, (self.p**2)*self.c, self.vector_size))
+        # Class token
+        self.class_token = nn.Parameter(torch.randn(1, 1, self.vector_size))
+        # Position embedding
+        self.pos_embedding = nn.Parameter(torch.randn(1, self.n_embeddings + 1, self.vector_size))
+
+        # Layer normalization as in CLIP (Radford et al. 2021)
+        self.layer_norm = nn.LayerNorm(self.vector_size)
+
+        # Transformer Encoder Hidden Layers
+        transformer_layer = nn.TransformerEncoderLayer(d_model=self.vector_size, activation='gelu', nhead=self.nhead, dim_feedforward=self.mlp_size, norm_first=True, batch_first=True)
+        self.transformer = nn.TransformerEncoder(encoder_layer=transformer_layer, num_layers=self.n_layers)
+        self.to_latent = nn.Identity()
+
+        # Out MLP head with one
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(self.vector_size),
+            nn.Linear(self.vector_size, self.mlp_size),
+            nn.Linear(self.mlp_size, dim_out),
+        )
+
+    def forward(self, x):
+        N, c, h, w = x.shape
+
+        # Convert image to patches
+        x = self.rearrange(x)
+
+        # Patch + Position Embedding
+        x = torch.matmul(x, self.patch_embedding)
+        class_token = self.class_token.expand(N, -1, -1)
+        x = torch.cat((class_token, x), dim=1)
+        x += self.pos_embedding
+        x = self.layer_norm(x)
+
+        # Transformer Encoder Layers
+        x = self.transformer(x)
+
+        # Getting class token
+        x = x[:, 0]
+        x = self.to_latent(x)
+
+        x = self.mlp_head(x)
+        return x
+
+
+
 # ViT-S/16 @ 112
 class ViTSmallOver16at112(nn.Module):
     def __init__(self, dim_out):
@@ -75,73 +145,6 @@ class ViTSmallOver16at112(nn.Module):
         return x
 
 
-# ViT-S/8 @ 112
-class ViTSmallOver8at112(nn.Module):
-    def __init__(self, dim_out):
-        super(ViTSmallOver8at112, self).__init__()
-        # ViT Hyper-parameters
-        self.c, self.h, self.w = (3, 112, 112)
-        self.p = 8
-
-        # Number of layers
-        self.n_layers = 8
-        # Latent vector size D
-        self.vector_size = 512
-        # FF dim
-        self.mlp_size = 2048
-        # Number of heads
-        self.nhead = 8
-
-        # Number of patches
-        self.n_embeddings = (self.h * self.w) // (self.p**2)
-
-        # Get patches
-        self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=self.p, p2=self.p)
-
-        # Patch embedding
-        self.patch_embedding = nn.Parameter(torch.randn(1, (self.p**2)*self.c, self.vector_size))
-        # Class token
-        self.class_token = nn.Parameter(torch.randn(1, 1, self.vector_size))
-        # Position embedding
-        self.pos_embedding = nn.Parameter(torch.randn(1, self.n_embeddings + 1, self.vector_size))
-
-        # Layer normalization as in CLIP (Radford et al. 2021)
-        self.layer_norm = nn.LayerNorm(self.vector_size)
-
-        # Transformer Encoder Hidden Layers
-        transformer_layer = nn.TransformerEncoderLayer(d_model=self.vector_size, activation='gelu', nhead=self.nhead, dim_feedforward=self.mlp_size, norm_first=True, batch_first=True)
-        self.transformer = nn.TransformerEncoder(encoder_layer=transformer_layer, num_layers=self.n_layers)
-        self.to_latent = nn.Identity()
-
-        # Out MLP head with one
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(self.vector_size),
-            nn.Linear(self.vector_size, self.mlp_size),
-            nn.Linear(self.mlp_size, dim_out),
-        )
-
-    def forward(self, x):
-        N, c, h, w = x.shape
-
-        # Convert image to patches
-        x = self.rearrange(x)
-
-        # Patch + Position Embedding
-        x = torch.matmul(x, self.patch_embedding)
-        class_token = self.class_token.expand(N, -1, -1)
-        x = torch.cat((class_token, x), dim=1)
-        x += self.pos_embedding
-        x = self.layer_norm(x)
-
-        # Transformer Encoder Layers
-        x = self.transformer(x)
-
-        # Getting class token
-        x = x[:, 0]
-        x = self.to_latent(x)
-
-        x = self.mlp_head(x)
-        return x
 
 
 # ViT-B/16 @ 112
