c1589a18b2fc127caee7081a65d65e1bd5ee065f
cs582 <cs582@duke.edu>
2023-05-12 13:52:43 +0800

    evaluation loop

--- src/eval_loop.py
+++ src/eval_loop.py
@@ -13,20 +13,24 @@ s3 = boto3.client('s3')
 models_dir = "src/models/checkpoints"
 
 
-def eval(eval_dataset, clip_model, loss_function, device, model_name, load_from_given_checkpoint=None):
+import torch
+
+def eval(eval_dataset, clip_model, loss_function, device, load_from_given_checkpoint=None):
     """
-    Training loop.
-    :param eval_dataset: (Dataloader) training data.
+    Evaluation loop.
+    :param eval_dataset: (Dataloader) evaluation data.
     :param clip_model: (torch.nn.Module) CLIP model.
     :param loss_function: (torch.nn.Module) CLIP loss function.
     :param device: (torch.device) Torch device.
-    :param model_name: (str) Image encoder name.
-    :param load_from_given_checkpoint: (str) Gives it a specific model to load.
-    :return:
+    :param load_from_given_checkpoint: (str) Path to the checkpoint to load.
+    :return: top1_accuracy, top5_accuracy
     """
     _, _ = load_from_checkpoint(load_from_given_checkpoint, clip_model)
 
     loss_eval = []
+    top1_correct = 0
+    top5_correct = 0
+    total_samples = 0
 
     pbar = tqdm(total=len(eval_dataset), desc="Eval. Loop.")
     for idx, (images, queries) in enumerate(eval_dataset):
@@ -39,6 +43,20 @@ def eval(eval_dataset, clip_model, loss_function, device, model_name, load_from_
         loss = loss_function(logits_images, logits_text)
         loss_eval.append(loss.item())
 
+        # Calculate Top-1 and Top-5 accuracies
+        predictions = torch.topk(logits_text, k=5, dim=1)[1]
+        labels = torch.arange(logits_text.size(0)).to(device)
+        top1_correct += torch.sum(predictions[:, 0] == labels).item()
+        top5_correct += torch.sum(predictions == labels.view(-1, 1)).item()
+        total_samples += labels.size(0)
+
         pbar.update(1)
 
-    print(f"Eval Loop DONE! AVG Loss: {np.round(np.mean(loss_eval), 5)}.")
+    pbar.close()
+
+    top1_accuracy = top1_correct / total_samples
+    top5_accuracy = top5_correct / total_samples
+
+    print(f"Top-1 Accuracy: {top1_accuracy:.4f}, Top-5 Accuracy: {top5_accuracy:.4f}")
+
+    return top1_accuracy, top5_accuracy
