16caa058af5fd7a5da758cf54672d6cd314cadf8
cs582 <cs582@duke.edu>
2023-05-14 12:14:27 +0800

    added GPT Small model

--- src/models/natural_language_processing/nlp_backbones.py
+++ src/models/natural_language_processing/nlp_backbones.py
@@ -9,6 +9,37 @@ from src.models.natural_language_processing.nlp_token_embedding import TokenEmbe
 additional_tokens = {"[SOS]": 43001, "[EOS]": 43000}
 
 
+class GPTSmall(nn.Module):
+    def __init__(self, dim_out, batch_size, vocab_size, max_length):
+        super(GPTSmall, self).__init__()
+        self.batch_size = batch_size
+        self.max_length = max_length
+
+        self.additional_tokens = additional_tokens
+
+        # The embedder takes size vocabulary_size+1 because it should ignore the dummy token 0
+        self.token_embedder = TokenEmbedder(vocabulary_size=vocab_size+1, embedding_dim=dim_out)
+        self.transformer = TextTransformer(dim_model=dim_out, n_layers=8, max_length=max_length, nhead=8, dim_ff=1024)
+
+        self.register_buffer('mask', torch.zeros(self.batch_size, self.max_length, self.max_length, dtype=torch.bool))
+        self.register_buffer('eos_mask', torch.zeros(self.batch_size, self.max_length, dtype=torch.bool))
+
+    def forward(self, x):
+        b, _ = x.shape
+
+        # Create masks
+        self.mask[:, :, :] = 0.0
+        self.eos_mask[:, :] = (x == self.additional_tokens["[EOS]"])
+        for small_b in range(b):
+            sentence_length = (x[small_b] != 0).sum()
+            self.mask[small_b, :sentence_length, :sentence_length] = torch.triu(torch.ones(sentence_length, sentence_length), diagonal=1).T
+        # Token embedder
+        x = self.token_embedder(x)
+        # Transformer backbone
+        x = self.transformer(x, self.mask, self.eos_mask)
+        return x
+
+
 class GPTBase(nn.Module):
     def __init__(self, dim_out, batch_size, vocab_size, max_length):
         super(GPTBase, self).__init__()
