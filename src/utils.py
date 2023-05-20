f9ad42719c716564517cb6f0bd1cb8936485ea7e
cs582 <cs582@duke.edu>
2023-05-20 15:05:15 +0800

    updated log message

--- src/utils.py
+++ src/utils.py
@@ -71,12 +71,13 @@ def load_from_checkpoint(model_filepath, model, scheduler=None, optimizer=None):
     return epoch, loss_history
 
 
-def training_info_log_message(device, epochs, batch_size, image_encoder, text_encoder, image_dim_out, text_dim_out, optimizer):
+def training_info_log_message(device, use_checkpoint, epochs, accumulate, batch_size, image_encoder, text_encoder, image_dim_out, text_dim_out, optimizer):
     """
     Prints the training loop information.
 
     :param device: (torch.device) device.
     :param epochs: (int) number of epochs.
+    :param accumulate: (int) number of batches to accumulate.
     :param batch_size: (int) batch size.
     :param image_encoder: (str) image encoder name.
     :param text_encoder: (str) text encoder name.
@@ -91,6 +92,8 @@ def training_info_log_message(device, epochs, batch_size, image_encoder, text_en
     ____________________________________________
     Device:         {torch.cuda.get_device_name() if device == torch.device("cuda:0") else "CPU"}
     Epochs:         {epochs}
+    Use Checkpoint: {use_checkpoint}
+    Accumulate:     {accumulate}
     Batch size:     {batch_size}
     Image Encoder:  ViT{image_encoder} (dim = {image_dim_out})
     Text Encoder:   {text_encoder}  (dim = {text_dim_out})
